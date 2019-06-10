64281e9250ac67f7d19f5a7c355376c5c21c62d5
root <root@rcsnas.rcs.al>
2019-06-10 15:56:08 +0200

    Add theme and binaries

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,14 +42,11 @@ TARGET_HW_DISK_ENCRYPTION := true
 
 ## Kernel
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 androidboot.selinux=permissive buildvariant=eng
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 
-#TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-TARGET_KERNEL_CONFIG := markw_defconfig
-TARGET_KERNEL_SOURCE := kernel/xiaomi/markw
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/Image.gz
 
 ## Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE     := 67108864
@@ -89,7 +86,7 @@ TW_INPUT_BLACKLIST := "hbtp_vm"
 
 TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
 
-TW_DEVICE_VERSION := 0-treble
+TW_DEVICE_VERSION := 1-markw
 
 ## Treble
 BOARD_NEEDS_VENDORIMAGE_SYMLINK := false
