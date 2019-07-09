6855c77f69822ee58f8ec63d8d6b41769dc941b0
redispade <34771407+redispade@users.noreply.github.com>
2019-07-09 18:11:15 +0200

    Switch to brigudav's kernel built from source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,8 +45,10 @@ BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 androidboot.selinux=permissive buildvariant=eng
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/Image.gz
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_CONFIG := markw_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/markw
+#TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/Image.gz
 
 ## Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE     := 67108864
