22e4e285bdf0d36cd0d74c714655129b5b9397e4
Redis <redis@rcs.al>
2019-07-12 22:20:14 +0000

    Pitchblack Recovery

--- omni_markw.mk
+++ omni_markw.mk
@@ -15,10 +15,10 @@
 #
 
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/pb/config/gsm.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit Telephony packages
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
