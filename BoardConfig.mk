696bd020b2dd46fc1b8fcf0ca0daec42c1e63f63
Redis <redis@rcs.al>
2020-05-17 02:28:20 +0200

    Remove symlinks, twrp handles system_root and system automatically now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,7 +71,6 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 
 #System as root
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
-BOARD_ROOT_EXTRA_SYMLINKS := /system_root/system:/system
 
 ## TWRP Configuration
 TW_THEME := portrait_hdpi
@@ -113,7 +112,7 @@ BOARD_ALWAYS_INSECURE := true
 #TW_HAS_DOWNLOAD_MODE = true
 #TW_HAS_EDL_MODE = true
 
-TW_DEVICE_VERSION := 14-markw
+TW_DEVICE_VERSION := 15-markw
 
 # supress error messages while building
 ALLOW_MISSING_DEPENDENCIES := true
