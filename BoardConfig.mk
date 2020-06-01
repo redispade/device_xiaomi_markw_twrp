1b287054582e9ba1f1b18bdd0cda4d5875f84e14
Redis <redis@rcs.al>
2020-06-01 12:55:39 +0200

    removed blob update to fix encryption, version update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -114,7 +114,7 @@ BOARD_ALWAYS_INSECURE := true
 #TW_HAS_DOWNLOAD_MODE = true
 #TW_HAS_EDL_MODE = true
 
-TW_DEVICE_VERSION := 16-markw
+TW_DEVICE_VERSION := 16a-markw
 
 # supress error messages while building
 ALLOW_MISSING_DEPENDENCIES := true
